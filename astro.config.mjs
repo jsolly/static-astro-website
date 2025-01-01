950d13e61a63ee9f1ea98f042a52c08cc59e2ba1
John Solly <9572232+jsolly@users.noreply.github.com>
2025-01-01 17:13:19 -0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://www.example.com",
 	integrations: [tailwind(), sitemap()],
 });
