bb2d8032dd9c9995e80aefc6b3bb4074c8188237
John Solly <9572232+jsolly@users.noreply.github.com>
2025-05-21 04:23:27 -0400

    .

--- config/astro.config.mjs
+++ config/astro.config.mjs
@@ -1,17 +1,24 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
-
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import sitemap from "@astrojs/sitemap";
+import vue from "@astrojs/vue";
+import tailwindcss from "@tailwindcss/vite";
+// @ts-nocheck
+import { defineConfig } from "astro/config";
+import dotenv from "dotenv";
+
+// Load environment variables from the root directory
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.example.com",
-	integrations: [
-		tailwind({
-			configFile: "./config/tailwind.config.mjs",
-		}),
-		sitemap(),
-	],
+	site:
+		process.env.NODE_ENV === "development"
+			? "http://localhost:4321"
+			: "https://www.example.com",
+	integrations: [sitemap({}), vue()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
